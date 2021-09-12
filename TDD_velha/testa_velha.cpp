ca9cd8204e923b5dfd84131009e14fb247c35681
Guilherme Fleury <guilherme.fleury26@gmail.com>
2021-09-12 16:39:04 -0300

    rows and columns tested and working
    set function tested and working, diagonal failing on main diagonal

--- TDD_velha/testa_velha.cpp
+++ TDD_velha/testa_velha.cpp
@@ -13,13 +13,59 @@ TEST_CASE("Testa velha")
 	int teste1[3][3] = {{2, 0, 1},
 						{2, 0, 1},
 						{0, 2, 1}};
-	REQUIRE(VerificaVelha(teste1) == 1);
+	REQUIRE(VerificaVelha(teste1) == 0);
 }
 
-TEST_CASE("win")
+TEST_CASE("x row win")
 {
-	int test2[3][3] = {{2, 2, 1},
-					   {0, 0, 0},
+	int test3[3][3] = {{0, 0, 0},
+					   {1, 1, 1},
 					   {2, 0, 1}};
-	REQUIRE(Verifywin(test2) == 1);
+	REQUIRE(VerifySet(test3, 1, 1) == 1);
+	REQUIRE(VerifySet(test3, 2, 1) == -1);
+}
+
+TEST_CASE("o row win")
+{
+	int test4[3][3] = {{0, 0, 0},
+					   {2, 2, 2},
+					   {1, 0, 1}};
+	REQUIRE(VerifySet(test4, 2, 1) == 2);
+	REQUIRE(VerifySet(test4, 1, 1) == -1);
+}
+
+TEST_CASE("x column win")
+{
+	int test5[3][3] = {{1, 0, 0},
+					   {1, 2, 2},
+					   {1, 0, 2}};
+	REQUIRE(VerifySet(test5, 1, 0) == 1);
+	REQUIRE(VerifySet(test5, 2, 0) == -1);
+}
+
+TEST_CASE("o column win")
+{
+	int test6[3][3] = {{2, 0, 0},
+					   {2, 1, 1},
+					   {2, 0, 1}};
+	REQUIRE(VerifySet(test6, 1, 0) == -1);
+	REQUIRE(VerifySet(test6, 2, 0) == 2);
+}
+
+TEST_CASE("both row win")
+{
+	int test7[3][3] = {{1, 1, 1},
+					   {2, 2, 2},
+					   {2, 0, 1}};
+	REQUIRE(VerifySet(test7, 1, 1) == 1);
+	REQUIRE(VerifySet(test7, 2, 1) == 2);
+}
+
+TEST_CASE("diagonal x win")
+{
+	int test8[3][3] = {{1, 2, 1},
+					   {2, 1, 2},
+					   {1, 2, 1}};
+	REQUIRE(VerifyDiagonal(test8, 1, 1) == 1);
+	REQUIRE(VerifyDiagonal(test8, 1, 0) == 1);
 }
--- TDD_velha/velha.hpp
+++ TDD_velha/velha.hpp
@@ -1,3 +1,7 @@
 int VerificaVelha(int velha[3][3]);
 
-int Verifywin(int win[3][3]);
+int Verifywin(int win[3][3], int i);
+
+int VerifySet(int win[3][3], int player, int r_or_c);
+
+int VerifyDiagonal(int win[3][3], int player, int diagonal);
