15bb72d335f4711638fcf0b9d1c3c6c1ecbc1862
Guilherme Fleury <guilherme.fleury26@gmail.com>
2021-09-11 22:22:06 -0300

    xtest implemented for columns
    not yet tested

--- TDD_velha/velha.cpp
+++ TDD_velha/velha.cpp
@@ -20,8 +20,8 @@ int VerificaVelha( int velha[3][3] )
 
 int VerifyXwin( int xwin[3][3])
 {
-	int c,i,j;
-	for(i=0;i<3;i++){
+	int c,i,j;  
+	for(i=0;i<3;i++){     /*line verifying loop*/
 	for (c=0,j=0; j<3; j++){
 		if (xwin[i][j] != 1){
 			j=3;
@@ -42,6 +42,29 @@ int VerifyXwin( int xwin[3][3])
 
 		
 	}
+
+	for(i=0;i<3;i++){       /*column verifying loop*/
+	for (c=0,j=0; j<3; j++){
+		if (xwin[j][i] != 1){
+			j=3;
+		}
+		if (xwin[j][i] == 1){
+			c++;
+
+		}
+		
+		
+
+	}
+		
+		
+		if (c == 3) {
+			return 1;
+		}
+
+		
+	}
+
 
 
 	return 0;
